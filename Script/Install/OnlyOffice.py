eaa2c24fafa37040f9b4c7355506d6caecf6824e
Mane <mane@manesec.com>
2023-06-02 15:18:36 +0800

    Update OnlyOffice.py

--- Script/Install/OnlyOffice.py
+++ Script/Install/OnlyOffice.py
@@ -9,5 +9,5 @@ def Run():
     Download_link = "https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb"
     Unit.WgetDownloadFile(Download_link, "/tmp/onlyoffice-desktopeditors_amd64.deb")
     os.system("apt install -y fonts-crosextra-carlito")
-    os.system("dpkg -i onlyoffice-desktopeditors_amd64.deb")
+    os.system("dpkg -i /tmp/onlyoffice-desktopeditors_amd64.deb")
     os.system("rm /tmp/onlyoffice-desktopeditors_amd64.deb")
